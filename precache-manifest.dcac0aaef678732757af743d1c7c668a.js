582313aadb5a30df9aa06c9d47c45cc730bb2c7e
Rahul-kalsha <56481473+Rahul-kalsha@users.noreply.github.com>
2022-11-15 10:16:15 +0000

    Deploying to gh-pages from @ Rahul-kalsha/react-demo-app@becc71097a40159bd4f87b07cac6913f541461ca 🚀

---
+++ precache-manifest.dcac0aaef678732757af743d1c7c668a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "527f66fe2470a76e8b10",
+    "url": "/react-demo-app/static/css/main.924adf4b.chunk.css"
+  },
+  {
+    "revision": "527f66fe2470a76e8b10",
+    "url": "/react-demo-app/static/js/main.527f66fe.chunk.js"
+  },
+  {
+    "revision": "30096658bfd556f39eb9",
+    "url": "/react-demo-app/static/js/1.30096658.chunk.js"
+  },
+  {
+    "revision": "95b17263998348bb1a9d",
+    "url": "/react-demo-app/static/js/2.95b17263.chunk.js"
+  },
+  {
+    "revision": "f7b8c2fd2de314c621e0",
+    "url": "/react-demo-app/static/js/runtime~main.f7b8c2fd.js"
+  },
+  {
+    "revision": "72657ab8dfc823f5043287d10444e331",
+    "url": "/react-demo-app/index.html"
+  }
+];
